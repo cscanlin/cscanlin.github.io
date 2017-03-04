93f60a4890c6038a069e3ad9454b6e7b24ebd604
Chris Scanlin <cscanlin@gmail.com>
2017-03-03 21:15:33 -0800

    fully working python generate_screenshot

--- generate_screenshots.py
+++ generate_screenshots.py
@@ -1,32 +1,77 @@
 import os
+import logging
 import requests
 from selenium import webdriver
-from urlparse import urlparse
+from urllib.parse import urlparse
+import __main__
 
-WIDTH, HEIGHT = 1024, 768
-REPOSITORY_URLS = [
-    'https://github.com/cscanlin/munger-builder',
-    'https://github.com/cscanlin/choice-optimizer',
-    'https://github.com/cscanlin/minesweeper',
-    'https://github.com/cscanlin/periodic-table-timeline',
-]
+default_logger = logging.getLogger(__main__.__file__)
 
-def retrieve_repo_data(repo_url):
-    api_path = 'https://api.github.com/repos{}'.format(urlparse(repo_url)['path'])
-    return requests.get(api_path).json()
+def get_log_handler():
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.INFO)
+    return handler
 
-def capture_screenshot(repo_data, output_directory='images'):
-    driver.get(repo_data['homepage'])
-    screenshot_path = os.path.join(output_directory, '{}.png'.format(repo_data['name']))
-    driver.save_screenshot(screenshot_path)
-    print('finished: {}'.format(repo_data['name']))
+class Repository(object):
+    REPO_API_PATH = 'https://api.github.com/repos'
+
+    def __init__(self, repo_url):
+        self.repo_url = repo_url
+        self.repo_data = self.retrieve_repo_data(repo_url)
+        self.screenshot = None
+
+    @staticmethod
+    def retrieve_repo_data(repo_url):
+        api_path = '{0}{1}'.format(Repository.REPO_API_PATH, urlparse(repo_url).path)
+        return requests.get(api_path).json()
+
+class Screenshotter(object):
+    def __init__(self,
+                 repository_urls,
+                 width=1280,
+                 height=800,
+                 output_directory='images',
+                 screenshot_format='png',
+                 logger=default_logger):
+        self.repositories = [Repository(url) for url in repository_urls]
+        self.width = width
+        self.height = height
+        self.output_directory = output_directory
+        self.screenshot_format = screenshot_format
+        self.logger = logger
+        self.logger.setLevel(logging.INFO)
+        self.logger.addHandler(get_log_handler())
+
+    def __enter__(self):
+        self.driver = webdriver.Firefox()
+        self.driver.set_window_size(self.width, self.height)
+        return self
+
+    def __exit__(self, *args):
+        self.driver.quit()
+
+    def capture_screenshot(self, repository):
+        self.driver.get(repository.repo_data['homepage'])
+        self.logger.info('finished: {}'.format(repository.repo_data['name']))
+        return self.driver.save_screenshot(self.screenshot_path(repository))
+
+    def screenshot_path(self, repository):
+        file_name = '{0}.{1}'.format(repository.repo_data['name'], self.screenshot_format)
+        return os.path.join(self.output_directory, file_name)
+
+    def run(self):
+        for repo in self.repositories:
+            repo.screenshot = self.capture_screenshot(repo)
+        self.logger.info('All finished!')
 
 if __name__ == '__main__':
-    driver = webdriver.PhantomJS()
-    driver.set_window_size(WIDTH, HEIGHT)
-    for url in REPOSITORY_URLS:
-        print(url)
-        repo_data = retrieve_repo_data(url)
-        print(repo_data)
-        capture_screenshot(repo_data)
-    driver.quit()
+    repository_urls = [
+        'https://github.com/cscanlin/munger-builder',
+        'https://github.com/cscanlin/choice-optimizer',
+        'https://github.com/cscanlin/minesweeper',
+        'https://github.com/cscanlin/periodic-table-timeline',
+    ]
+    with Screenshotter(repository_urls) as ss:
+        ss.run()
