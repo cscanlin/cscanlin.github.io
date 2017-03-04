9423d520916d042f30c513ad82e0ebdb6bee4fdb
Chris Scanlin <cscanlin@gmail.com>
2017-03-04 00:04:22 -0800

    add datetime to filename, add clear_screenshot_directory, refactor Repository class

--- generate_screenshots.py
+++ generate_screenshots.py
@@ -1,7 +1,9 @@
 import os
 import logging
 import requests
+import shutil
 import yaml
+from datetime import datetime
 from selenium import webdriver
 from urllib.parse import urlparse
 import __main__
@@ -18,15 +20,15 @@ def get_log_handler():
 class Repository(object):
     REPO_API_PATH = 'https://api.github.com/repos'
 
-    def __init__(self, repo_url):
-        self.repo_url = repo_url
-        self.repo_data = self.retrieve_repo_data(repo_url)
+    def __init__(self, repo_data):
         self.screenshot = None
+        for k, v in repo_data.items():
+            setattr(self, k, v)
 
-    @staticmethod
-    def retrieve_repo_data(repo_url):
+    @classmethod
+    def from_url(cls, repo_url):
         api_path = '{0}{1}'.format(Repository.REPO_API_PATH, urlparse(repo_url).path)
-        return requests.get(api_path).json()
+        return cls(requests.get(api_path).json())
 
 class Screenshotter(object):
     def __init__(self,
@@ -37,7 +39,7 @@ class Screenshotter(object):
                  screenshot_format='png',
                  data_dump_directory='_data',
                  logger=default_logger):
-        self.repositories = [Repository(url) for url in repository_urls]
+        self.repositories = [Repository.from_url(url) for url in repository_urls]
         self.width = width
         self.height = height
         self.screenshot_directory = screenshot_directory
@@ -47,6 +49,7 @@ class Screenshotter(object):
         self.logger = logger
         self.logger.setLevel(logging.INFO)
         self.logger.addHandler(get_log_handler())
+        self.init_time = datetime.utcnow().replace(microsecond=0).isoformat()
 
     def __enter__(self):
         self.driver = webdriver.Firefox()
@@ -56,22 +59,32 @@ class Screenshotter(object):
     def __exit__(self, *args):
         self.driver.quit()
 
+    def clear_screenshot_directory(self):
+        for f in os.listdir(self.screenshot_directory):
+            if not f.endswith('.keep'):
+                os.remove(os.path.join(self.screenshot_directory, f))
+
     def capture_screenshot(self, repository):
-        self.driver.get(repository.repo_data['homepage'])
-        self.logger.info('finished: {}'.format(repository.repo_data['name']))
+        self.driver.get(repository.homepage)
+        self.logger.info('finished: {}'.format(repository.name))
         return self.driver.save_screenshot(self.screenshot_path(repository))
 
     def screenshot_path(self, repository):
-        file_name = '{0}.{1}'.format(repository.repo_data['name'], self.screenshot_format)
+        file_name = '{}_{}.{}'.format(
+            repository.name,
+            self.init_time,
+            self.screenshot_format,
+        )
         return os.path.join(self.screenshot_directory, file_name)
 
     def run(self):
+        self.clear_screenshot_directory()
         for repo in self.repositories:
             repo.screenshot = self.capture_screenshot(repo)
         self.logger.info('All finished!')
 
     def dump_repo_data(self):
-        formatted_data = {repo.repo_data['name']: repo.repo_data for repo in self.repositories}
+        formatted_data = {repo.name: repo.__dict__ for repo in self.repositories}
         export_path = os.path.join(self.data_dump_directory, 'repo_data.yml')
         with open(export_path, 'w') as ef:
             yaml.dump(formatted_data, ef, default_flow_style=False)
