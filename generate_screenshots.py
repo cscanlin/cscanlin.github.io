22c9d22e0efd0aac7c75658837169858e75acf0d
Chris Scanlin <cscanlin@gmail.com>
2017-03-04 17:03:21 -0800

    add support for alternative target than homepage

--- generate_screenshots.py
+++ generate_screenshots.py
@@ -19,19 +19,23 @@ def get_log_handler():
 class Repository(object):
     REPO_API_PATH = 'https://api.github.com/repos'
 
-    def __init__(self, repo_data):
+    def __init__(self, repo_data, screenshot_target=None):
+        self.screenshot_target = screenshot_target if screenshot_target else repo_data['homepage']
         self.screenshot = None
         for k, v in repo_data.items():
             setattr(self, k, v)
 
     @classmethod
-    def retrieve_from_url(cls, repo_url):
-        api_path = '{0}{1}'.format(Repository.REPO_API_PATH, urlparse(repo_url).path)
-        repo_data = requests.get(api_path).json()
-        return cls(repo_data)
+    def retrieve_from_url(cls, repo_url, screenshot_target=None, allow_request=True):
+        if allow_request:
+            api_path = '{0}{1}'.format(Repository.REPO_API_PATH, urlparse(repo_url).path)
+            repo_data = requests.get(api_path).json()
+        else:
+            repo_data = cls.parse_data_from_url(repo_url)
+        return cls(repo_data, screenshot_target)
 
-    @classmethod
-    def parse_from_url(cls, repo_url):
+    @staticmethod
+    def parse_data_from_url(repo_url):
         repo_data = {
             'name': repo_url.split('/')[-1],
             'html_url': repo_url,
@@ -39,17 +43,16 @@ class Repository(object):
         repo_data['homepage'] = 'https://cscanlin.github.io/{}'.format(repo_data['name'])
         return repo_data
 
-
 class Screenshotter(object):
     def __init__(self,
-                 repository_urls,
+                 repositories,
                  width=1280,
                  height=800,
                  screenshot_directory='screenshots',
                  screenshot_format='png',
                  data_dump_directory='_data',
                  logger=default_logger):
-        self.repositories = [Repository.retrieve_from_url(url) for url in repository_urls]
+        self.repositories = repositories
         self.width = width
         self.height = height
         self.screenshot_directory = screenshot_directory
@@ -69,13 +72,18 @@ class Screenshotter(object):
     def __exit__(self, *args):
         self.driver.quit()
 
+    @classmethod
+    def from_urls(cls, repository_urls):
+        repo_target_pairs = [r if isinstance(r, tuple) else (r, None) for r in repository_urls]
+        return cls([Repository.retrieve_from_url(url, target) for url, target in repo_target_pairs])
+
     def clear_screenshot_directory(self):
         for f in os.listdir(self.screenshot_directory):
             if not f.endswith('.keep'):
                 os.remove(os.path.join(self.screenshot_directory, f))
 
     def capture_screenshot(self, repository):
-        self.driver.get(repository.homepage)
+        self.driver.get(repository.screenshot_target)
         self.logger.info('finished: {}'.format(repository.name))
         filename = self.screenshot_filename(repository)
         self.driver.save_screenshot(
@@ -105,11 +113,14 @@ class Screenshotter(object):
 
 if __name__ == '__main__':
     repository_urls = [
-        'https://github.com/cscanlin/munger-builder',
+        (
+            'https://github.com/cscanlin/munger-builder',
+            'http://www.mungerbuilder.com/script_builder/pivot_builder/1',
+        ),
         'https://github.com/cscanlin/choice-optimizer',
         'https://github.com/cscanlin/minesweeper',
         'https://github.com/cscanlin/periodic-table-timeline',
     ]
-    with Screenshotter(repository_urls) as ss:
+    with Screenshotter.from_urls(repository_urls) as ss:
         ss.run()
         ss.dump_repo_data()
